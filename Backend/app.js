7d843537b3c2fd3e39df065273b70ad3b380d106
= <=>
2026-02-28 19:28:42 +0100

    Create app and server file

---
+++ Backend/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+mongoose
+  .connect("mongodb://localhost:27017/grimoire")
+  .then(() => {
+    console.log("Connect to mongoose");
+  })
+  .catch((e) => {
+    console.error(e);
+  });
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-type, Authorization, Accept",
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, DELETE, PUT, PATCH, OPTIONS",
+  );
+  next();
+});
+module.exports = app;
